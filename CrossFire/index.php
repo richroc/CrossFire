2368c3ac4cd3e3aebfe35ffd586811c648aca6b4
Richard Tafoya <richroc17@gmail.com>
2014-06-25 17:03:50 -0700

    Updated index.php file to correct use of ps.

--- CrossFire/index.php
+++ CrossFire/index.php
@@ -43,33 +43,33 @@
 					echo "Autostart  <disabled>disabled</disabled> | <a href=\"karma/autoKarmaStart.php\"><b>Start</b></a><br />";
 				}
 
-				$cronjobs = ( exec("ps -all | grep [c]ron"));
+				$cronjobs = ( exec("ps | grep [c]ron"));
 				if ($cronjobs != ""){
 					echo "Cron Jobs <enabled>enabled</enabled>&nbsp; | <a href=\"jobs.php?stop&goback\"><b>Stop</b></a><br />";
 				} else {
 					echo "Cron Jobs <disabled>disabled</disabled> | <a href=\"jobs.php?start&goback\"><b>Start</b></a> | <a href=\"jobs.php\"><b>Edit</b></a><br />";
 				}
 
-				$isurlsnarfup = exec("ps auxww | grep urlsnarf.sh | grep -v -e grep");
+				$isurlsnarfup = exec("ps w | grep urlsnarf.sh | grep -v -e grep");
 				if ($isurlsnarfup != "") {
 					echo "URL Snarf  <enabled>enabled</enabled>&nbsp; | <a href=\"urlsnarf/stopurlsnarf.php\"><b>Stop</b></a><br />";
 				} else {
 					echo "URL Snarf  <disabled>disabled</disabled> | <a href=\"urlsnarf/starturlsnarf.php\"><b>Start</b></a><br />";
 				}
 
-				$isdnsspoofup = exec("ps auxww| grep dnsspoof.sh | grep -v -e grep");
+				$isdnsspoofup = exec("ps w| grep dnsspoof.sh | grep -v -e grep");
 				if ($isdnsspoofup != "") {
 					echo "DNS Spoof  <enabled>enabled</enabled>&nbsp; | <a href=\"dnsspoof/stopdnsspoof.php\"><b>Stop</b></a><br />";
 				} else {
 					echo "DNS Spoof  <disabled>disabled</disabled> | <a href=\"dnsspoof/startdnsspoof.php\"><b>Start</b></a> | <a href=\"config.php#spoofhost\"><b>Edit</b></a><br/>";
 				}
 
-				/*$isngrepup = exec("/bin/busybox ps | grep ngrep | grep -v -e \"grep ngrep\" | awk '{print $1}'");
+				$isngrepup = exec("ps | grep ngrep | awk '{print $1}'");
 				if ($isngrepup != "") {
 					echo "&nbsp;&nbsp;&nbsp;&nbsp;ngrep  <enabled>enabled</enabled>&nbsp; | <a href=\"ngrep/stopngrep.php\"><b>Stop</b></a>";
 				} else {
 					echo "&nbsp;&nbsp;&nbsp;&nbsp;ngrep  <disabled>disabled</disabled> | <a href=\"ngrep/startngrep.php\"><b>Start</b></a> | <a href=\"config.php#ngrep\"><b>Edit</b></a><br/>";
-				}*/
+				}
 
 				if (exec("grep 3g.sh /etc/rc.local") != ""){
 					echo "3G bootup  <enabled>enabled</enabled>&nbsp; | <a href=\"3g.php?disable&disablekeepalive&goback\"><b>Disable</b></a><br />";
@@ -83,7 +83,7 @@
 					echo "3G redial <font color='lime'><b>enabled</b></font>&nbsp; | <a href='3g.php?disablekeepalive&goback'><b>Disable</b></a><br />";
 				}
 				
-				if (exec("ps aux | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
+				if (exec("ps w | grep [s]sh | grep -v -e ssh.php | grep -v grep") == "") {
 					echo "&nbsp; &nbsp; &nbsp; SSH <disabled>offline</disabled> &nbsp;| <a href=\"ssh.php?connect\"><b>Connect</b></a><br /><br />";
 				} else {
 					echo "&nbsp; &nbsp; &nbsp; SSH <enabled>online</enabled> &nbsp; | <a href=\"ssh.php?disconnect\"><b>Disconnect</b></a><br /><br />";
@@ -95,6 +95,7 @@
 			<?php
 				echo "&nbsp;LAN/WAN Port: " . exec("ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'") . "<br />";
 				echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3G Modem: " . exec("ifconfig 3g-wan2 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'") . "<br />";
+				echo "&nbsp;BRIDGE Port: " . exec("ifconfig br-lan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1'") . "<br />;
 				echo "&nbsp; External IP: "; 
 				if (isset($_GET[revealpublic])) {
 					echo exec("wget -qO- http://cloud.wifipineapple.com/ip.php") . "<br />";
